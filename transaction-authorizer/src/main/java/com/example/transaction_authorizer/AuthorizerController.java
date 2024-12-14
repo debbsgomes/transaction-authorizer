e6086f3c35336bba7c4c750615cec4f6dcc24eeb
Deborah Gomes Souza <deborah270691@gmail.com>
2024-12-14 00:38:12 -0300

    authorizer controller with fallback

--- transaction-authorizer/src/main/java/com/example/transaction_authorizer/AuthorizerController.java
+++ transaction-authorizer/src/main/java/com/example/transaction_authorizer/AuthorizerController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthorizerController {
+    private double foodBalance = 1000.0;
+    private double mealBalance = 500.0;
+    private double cashBalance = 2000.0;
     @PostMapping("/authorize")
     public ResponseEntity<String> authorize(@RequestBody Transaction transaction) {
         String benefitCategory;
@@ -18,7 +21,32 @@ public class AuthorizerController {
             benefitCategory = "CASH";
         }
 
-        String responseCode = "00";
+        double balance;
+        if (benefitCategory.equals("FOOD")) {
+            balance = foodBalance;
+        } else if (benefitCategory.equals("MEAL")) {
+            balance = mealBalance;
+        } else {
+            balance = cashBalance;
+        }
+
+        String responseCode;
+        if (balance >= transaction.getAmount()) {
+            responseCode = "00";
+            if (benefitCategory.equals("FOOD")) {
+                foodBalance -= transaction.getAmount();
+            } else if (benefitCategory.equals("MEAL")) {
+                mealBalance -= transaction.getAmount();
+            } else {
+                cashBalance -= transaction.getAmount();
+            }
+        } else if (cashBalance >= transaction.getAmount()) {
+            responseCode = "00";
+            cashBalance -= transaction.getAmount();
+        } else {
+            responseCode = "51";
+        }
+
         String responseBody = "{\"code\": \"" + responseCode + "\"}";
         return ResponseEntity.ok(responseBody);
     }
