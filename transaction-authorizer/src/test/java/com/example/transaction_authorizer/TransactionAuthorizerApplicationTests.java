346ed6cf07919fb4f811540063341ae7600636df
Deborah Gomes Souza <deborah270691@gmail.com>
2024-12-13 21:45:12 -0300

    initial project commit

---
+++ transaction-authorizer/src/test/java/com/example/transaction_authorizer/TransactionAuthorizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.transaction_authorizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionAuthorizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
