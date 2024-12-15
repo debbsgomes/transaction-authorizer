90c49e3d8b1aeb94b52c54e32286063933ae0783
Deborah Gomes Souza <deborah270691@gmail.com>
2024-12-15 19:16:43 -0300

    merchant dependent logic

--- transaction-authorizer/src/main/java/com/example/transaction_authorizer/AuthorizerController.java
+++ transaction-authorizer/src/main/java/com/example/transaction_authorizer/AuthorizerController.java
@@ -4,21 +4,35 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
+import java.util.HashMap;
 
 @RestController
 public class AuthorizerController {
     private double foodBalance = 1000.0;
     private double mealBalance = 500.0;
     private double cashBalance = 2000.0;
+    private Map<String, String> merchantCategoryMap = new HashMap<>();
+    public AuthorizerController() {
+        merchantCategoryMap.put("UBER EATS", "MEAL");
+        merchantCategoryMap.put("UBER TRIP", "CASH");
+        merchantCategoryMap.put("PAG*JoseDaSilva", "CASH");
+        merchantCategoryMap.put("PICPAY*BILHETEUNICO", "CASH");
+    }
+
+
     @PostMapping("/authorize")
     public ResponseEntity<String> authorize(@RequestBody Transaction transaction) {
-        String benefitCategory;
-        if (transaction.getMcc().equals("5411") || transaction.getMcc().equals("5412")) {
-            benefitCategory = "FOOD";
-        } else if (transaction.getMcc().equals("5811") || transaction.getMcc().equals("5812")) {
-            benefitCategory = "MEAL";
-        } else {
-            benefitCategory = "CASH";
+        String benefitCategory = merchantCategoryMap.getOrDefault(transaction.getMerchant(), null);
+
+        if (benefitCategory == null) {
+            if (transaction.getMcc().equals("5411") || transaction.getMcc().equals("5412")) {
+                benefitCategory = "FOOD";
+            } else if (transaction.getMcc().equals("5811") || transaction.getMcc().equals("5812")) {
+                benefitCategory = "MEAL";
+            } else {
+                benefitCategory = "CASH";
+            }
         }
 
         double balance;
