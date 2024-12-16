209db701af0caef5b682eb3a3f02cc491631d301
Deborah Gomes Souza <deborah270691@gmail.com>
2024-12-16 11:51:52 -0300

    logging on transaction authorizer

--- transaction-authorizer/src/main/java/com/example/transaction_authorizer/TransactionAuthorizerApplication.java
+++ transaction-authorizer/src/main/java/com/example/transaction_authorizer/TransactionAuthorizerApplication.java
@@ -1,11 +1,20 @@
 package com.example.transaction_authorizer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TransactionAuthorizerApplication {
+	private static final Logger logger = LoggerFactory.getLogger(TransactionAuthorizerApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(TransactionAuthorizerApplication.class, args);
+		try {
+			SpringApplication.run(TransactionAuthorizerApplication.class, args);
+			logger.info("Transaction Authorizer Application started successfully");
+		} catch (Exception e) {
+			logger.error("Error occurred while starting Transaction Authorizer Application", e);
+		}
 	}
 }
